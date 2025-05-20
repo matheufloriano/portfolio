8a15fc98493833a44e530422de0f3ed38426646e
matheufloriano <matheusfelipe.mf00@gmail.com>
2025-05-19 23:38:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem() {
+  const areaMensagem = document.getElementById("mensagem");
+  areaMensagem.innerText = '“Se o machado está cego e não foi afiado, é preciso redobrar a força; agir com sabedoria assegura o sucesso.” (Eclesiastes 10:10)';
+  areaMensagem.style.display = "block";
+}
